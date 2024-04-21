073f9050e82d341cf7667a4e336f6d5cbfdb1180
hayyrs66 <rayrtsx@proton.me>
2024-04-21 13:57:35 -0600

    nvim config added

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,22 @@
+
+return {
+  "NvChad/nvim-colorizer.lua",
+
+  config = function()
+
+  require('colorizer').setup({
+  filetypes = { "*", "!TelescopePrompt", "!vim" },
+  user_default_options = {
+    RGB = true, -- #RGB hex codes
+    RRGGBB = true, -- #RRGGBB hex codes
+    RRGGBBAA = true, -- #RRGGBBAA hex codes
+    rgb_fn = true, -- CSS rgb() and rgba() functions
+    hsl_fn = true, -- CSS hsl() and hsla() functions
+    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn = true, -- Enable all CSS *functions*, but not the *values*
+    mode = "background", -- Set the display mode.
+  },
+})
+end
+}
+
